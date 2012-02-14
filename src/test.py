5bae4ac7e783d81d692e0901edf69666e36ebd8f
Ravindra Nath Kakarla <ravindhranath@gmail.com>
2012-02-14 20:49:10 +0530

    Updated test.py to use FeatureExtractor

--- src/test.py
+++ src/test.py
@@ -1,23 +1,19 @@
-from Tokenizer import Tokenizer
 from ReviewParser import ReviewParser
-from POSTagger import POSTagger
-import nltk
+from FeatureExtractor import FeatureExtractor
 
-rev  = ReviewParser(open('../data/reviews/Apple_iPhone_4.csv', 'rb'), 'CSV')
-tokenize_patterns = ['[Nn]ikon ?[dD][0-9]+', '([0-9]+ ?mm)', '(auto[ -_]focus)', '(Apple)? ?iphone ?[0-5]?[gs]*']
-
-w = Tokenizer(tokenize_patterns)
+review_file = ['Apple_iPhone_4.csv', 'Blackberry_Torch_9800.csv', 'Nikon_D90.csv', 'Canon_ELPH_300_HS.csv']
+rev = ReviewParser(open('../data/reviews/' + review_file[3], 'rb',), 'CSV')
 
 rev.parse()
 
 text = rev.get_raw_text()
 
-text = w.tokenize(text)
+f = FeatureExtractor(text)
 
-p = POSTagger(text)
+print f.get_frequent_features(5)
 
-tags = p.nltk_tag()
-
+"""
+#tokenize_patterns = ['[Nn]ikon ?[dD][0-9]+', '([0-9]+ ?mm)', '(auto[ -_]?focus)', '(Apple)[ ]?(iphone)??[0-5]?[gs]*']
 features = [w.lower() for (w,t) in tags if t.startswith('N') and t != 'NNP']
 
 features = p.stemmer(features)
@@ -29,3 +25,4 @@ obs = [ob for ob in dist.iteritems()]
 logfile = open('/tmp/log.txt', 'w')
 logfile.write("".join(str(obs)).replace("), (", ")\n("))
 logfile.close()
+"""
