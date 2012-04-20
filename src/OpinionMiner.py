d2820f475c742912b7d053acb6098df0c65e06c5
Ravindra Nath Kakarla <ravindhranath@gmail.com>
2012-04-20 14:16:11 +0530

    Updated OpinionMiner to respond to web requests

--- src/OpinionMiner.py
+++ src/OpinionMiner.py
@@ -3,6 +3,7 @@ from ReviewParser import ReviewParser
 from FeatureExtractor import FeatureExtractor
 from subprocess import check_output
 from OpinionSentenceFinder import OpinionSentenceFinder
+from math import ceil
 import sys, pprint
 import settings
 
@@ -12,10 +13,12 @@ class OpinionMiner:
 		self.cid = cid
 		self.min_support = min_support
 		self.ratings = {}
+		self.overall_rating = 0
 		self.features = []
 
 	def run(self):
-		rev = ReviewParser(open(settings.reviews_path + ReviewParser.map_cid_to_name(self.cid), 'rb',), review_files[choice].split('.')[-1])
+		name = ReviewParser.map_cid_to_name(self.cid)
+		rev = ReviewParser(open(settings.reviews_path + name, 'rb'), name.split('.')[-1])
 		rev.parse()
 
 		print "Mining", len(rev.reviews), "reviews"
@@ -41,20 +44,34 @@ class OpinionMiner:
 
 		opinion_sents = temp
 		
+		rating = 0.0
+		no_opinions = 0
 		for ftr, sentiment in opinion_sents:
 			if sentiment[0] is True:
 				self.ratings[ftr]['positive'] += 1
+				rating += 1
+				no_opinions += 1
 			elif sentiment[0] is False:
 				self.ratings[ftr]['negative'] += 1
+				rating -= 1
+				no_opinions += 1
 			else:
 				self.ratings[ftr]['neutral'] += 1
+
+		self.overall_rating = ceil(rating / no_opinions * 5)
 
 		pp = pprint.PrettyPrinter(indent = 4)
 		print "Is this a %s?" % f.product_category
 		print "%d features are interesting" % len(self.features)
 
 		#pp.pprint(opinion_sents)
-		pp.pprint(self.ratings)
+		response = {'ratings': self.ratings}
+		response['product_name'] = f.product_name
+		response['product_category'] = f.product_category.title()
+		response['overall_rating'] = self.overall_rating
+		response['best_sentence'] = ""
+		
+		return response
 
 
 if __name__ == "__main__":
@@ -72,5 +89,5 @@ if __name__ == "__main__":
 
 	cid = review_files[choice].split(".")[0].split("_")[-2] 
 	om = OpinionMiner(cid, min_support)
-	om.run()
+	print om.run()
 
